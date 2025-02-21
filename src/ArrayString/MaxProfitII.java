6fc66109186dc4b13dec726ec590bab5215174a6
jefferson <jefferson@backbase.com>
2025-02-20 22:50:50 -0300

    20/02/2025

--- src/ArrayString/MaxProfitII.java
+++ src/ArrayString/MaxProfitII.java
@@ -1,15 +1,17 @@
 package ArrayString;
 
-public class MaxProfit {
+public class MaxProfitII {
     public static int maxProfit(int[] prices) {
-        int buy = prices[0];
         int maxProfit = 0;
-
+        int buy = prices[0];
+        //[7, 1, 4, 2, 5, 4]
+        //[1,2,3,4,5]
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] < buy) {
                 buy = prices[i];
-            } else if (prices[i] - buy > maxProfit){
-                maxProfit = prices[i] - buy;
+            } else if (prices[i] > buy) {
+                maxProfit += prices[i] - buy;
+                buy = prices[i];
             }
         }
 
